157075fcb4b7edce9d9fd3b657ce55e711038493
Maria Dennise <101153388+dennisemed@users.noreply.github.com>
2022-08-03 02:41:39 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,72 +13,10 @@ mongoose.connect(process.env.DATABASE_URL)
 
 mongoose.connection.on("connected", function () {
 	console.log("Connected to Database "+"test");
-  });
+});
 
 const routes = require("./routes/api");
 app.use("/", routes);
-
-// app.post('/api/login', async (req, res) => {
-// 	const user = await User.findOne({
-// 		email: req.body.email,
-// 	})
-
-// 	if (!user) {
-// 		return { status: 'error', error: 'Usuário não cadastrado' }
-// 	}
-
-// 	const isPasswordValid = await bcrypt.compare(
-// 		req.body.password,
-// 		user.password
-// 	)
-
-// 	if (isPasswordValid) {
-// 		const token = jwt.sign(
-// 			{
-// 				name: user.name,
-// 				email: user.email,
-// 			},
-// 			'secret123'
-// 		)
-
-// 		return res.json({ status: 'ok', user: token })
-// 	} else {
-// 		return res.json({ status: 'error', user: false })
-// 	}
-// })
-
-// app.get('/api/lead', async (req, res) => {
-// 	const token = req.headers['x-access-token']
-
-// 	try {
-// 		const decoded = jwt.verify(token, 'secret123')
-// 		const email = decoded.email
-// 		const user = await User.findOne({ email: email })
-
-// 		return res.json({ status: 'ok', lead: user.lead })
-// 	} catch (error) {
-// 		console.log(error)
-// 		// res.json({ status: 'error', error: 'invalid token' })
-// 	}
-// })
-
-// app.post('/api/lead', async (req, res) => {
-// 	const token = req.headers['x-access-token']
-
-// 	try {
-// 		const decoded = jwt.verify(token, 'secret123')
-// 		const email = decoded.email
-// 		await User.updateOne(
-// 			{ email: email },
-// 			{ $set: { lead: req.body.lead } }
-// 		)
-
-// 		return res.json({ status: 'ok' })
-// 	} catch (error) {
-// 		console.log(error)
-// 		res.json({ status: 'error', error: 'invalid token' })
-// 	}
-// })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
