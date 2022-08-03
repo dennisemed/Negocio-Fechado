2efc2a6e52e3780c34cb6890d3c47b0f558ef710
Maria Dennise <101153388+dennisemed@users.noreply.github.com>
2022-08-02 23:15:36 -0300

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -32,72 +32,49 @@ function App() {
 	}
 
 	return (
-		
 		<section>
-        <div className="px-4 py-5 px-md-5 text-center text-md-start">
-            <div className="container">
-                <div className="row gx-md-5 align-items-center">
-                    <div className="col-md-6 mb-5 mb-md-0">
-                        <div className="nav-brand">
-                            <a className="navbar-brand text-uppercase" href="/"><img src={logo} alt="logomarca da CRM Negócio Fechado"/>
-							negócio fechado
-                            </a>
-                        </div>
-                        <h1 className="my-4 fw-bold ls-tight">
-                        As melhores soluções <br/>
-                        <span>para sua empresa</span>
-                        </h1>
-                    </div>
-                
-                <div className="col-md-6 mb-5 mb-md-0">
-                    <div className="card">
-                        <div className="card-body py-5 px-md-5">
-                            <form onSubmit={loginUser}>
-                                
-                                <div className="form-outline mb-3">
-                                    <input type="email" id="form3Example3" className="form-control" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="exemplo@email.com"/>
-                                    <label className="form-label" htmlFor="form3Example3">Email</label>
-                                </div>
-                                <div className="form-outline mb-4">
-                                    <input type="password" id="password-login" className="form-control" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                                    <label className="form-label" htmlFor="form3Example4">Senha</label>
-                                </div>
-                                <div className="row mb-4">
-                                    <div className="d-flex justify-content-start">
-                                    	<p>Não tem uma conta? <a href="/register">Clique aqui</a></p>
-                                    </div>
-                                </div>
-                                    <input type="submit" className="btn btn-primary btn-block text-uppercase mb-4" value="Entrar" />
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-		</div>
-    </section>
-		
+			<div className="px-4 py-5 px-md-5 text-center text-md-start">
+			    <div className="container">
+				<div className="row gx-md-5 align-items-center">
+				    <div className="col-md-6 mb-5 mb-md-0">
+					<div className="nav-brand">
+					    <a className="navbar-brand text-uppercase" href="/"><img src={logo} alt="logomarca da CRM Negócio Fechado"/>
+									negócio fechado
+					    </a>
+					</div>
+					<h1 className="my-4 fw-bold ls-tight">
+					As melhores soluções <br/>
+					<span>para sua empresa</span>
+					</h1>
+				    </div>
 
-		// <>
-		// 	<h1>Login</h1>
-		// 	<form onSubmit={loginUser} >
-		// 		<input
-		// 			value={email}
-		// 			onChange={(e) => setEmail(e.target.value)}
-		// 			type="email"
-		// 			placeholder="Email"
-		// 		/>
-		// 		<br />
-		// 		<input
-		// 			value={password}
-		// 			onChange={(e) => setPassword(e.target.value)}
-		// 			type="password"
-		// 			placeholder="Password"
-		// 		/>
-		// 		<br />
-		// 		<input type="submit" value="Login" />
-		// 	</form>
-		// </>
+				<div className="col-md-6 mb-5 mb-md-0">
+				    <div className="card">
+					<div className="card-body py-5 px-md-5">
+					    <form onSubmit={loginUser}>
+
+						<div className="form-outline mb-3">
+						    <input type="email" id="form3Example3" className="form-control" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="exemplo@email.com"/>
+						    <label className="form-label" htmlFor="form3Example3">Email</label>
+						</div>
+						<div className="form-outline mb-4">
+						    <input type="password" id="password-login" className="form-control" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+						    <label className="form-label" htmlFor="form3Example4">Senha</label>
+						</div>
+						<div className="row mb-4">
+						    <div className="d-flex justify-content-start">
+							<p>Não tem uma conta? <a href="/register">Clique aqui</a></p>
+						    </div>
+						</div>
+						    <input type="submit" className="btn btn-primary btn-block text-uppercase mb-4" value="Entrar" />
+					    </form>
+					</div>
+				    </div>
+				</div>
+			    </div>
+			</div>
+		</div>
+    	</section>
 	)
 }
 
